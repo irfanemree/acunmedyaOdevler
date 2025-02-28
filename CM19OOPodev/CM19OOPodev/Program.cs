30e3e7182647cd1f32ab4cc75fca861f3af87762
irfanemree <irfan.emre.tekeli.06@gmail.com>
2025-02-28 18:34:25 +0300

    Ödev1 yapıldı.

---
+++ CM19OOPodev/CM19OOPodev/Program.cs
@@ -0,0 +1,26 @@
+﻿using CM19OOPodev.Classes;
+
+Araba a = new Araba();
+a.marka = "BMW";
+a.vites = "Manuel";
+a.kasaTipi = "Sedan";
+a.ekranaYazdir();
+
+Araba m = new Araba();
+m.ekranaYazdir("Mercedes" , "Otomatik" , "Station Wagon");
+
+Araba au = new Araba();
+au.ekranaYazdir("Audi", "Otomatik", "Hatchback");
+
+Araba t = new Araba();
+t.ekranaYazdir("Toyota", "Manuel", "Sedan");
+
+Araba tog = new Araba();
+tog.ekranaYazdir("TOGG", "Otomatik", "SUV");
+
+Araba p = new Araba();
+p.ekranaYazdir("Porsche", "Otomatik", "SUV");
+
+
+
+
