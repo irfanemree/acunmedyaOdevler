197b31378eb41d0e6184044cfebfe6afba38ddd4
irfanemree <irfan.emre.tekeli.06@gmail.com>
2025-03-07 02:38:50 +0300

    28.02.2025-CM  Ödev 2

---
+++ CM19OOP_Sirket/CM19OOP_Sirket/Classes/Stajyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM19OOP_Sirket.Classes
+{
+    public class Stajyer : Calisan
+    {
+        public override double maasinizNedir()
+        {
+            return 5000;
+        }
+    }
+}
---
+++ CM19OOP_Sirket/CM19OOP_Sirket/Program.cs
@@ -0,0 +1,10 @@
+﻿using CM19OOP_Sirket.Classes;
+
+GenelMudur gm = new GenelMudur();
+Mudur m = new Mudur();
+Programci pr = new Programci();
+Stajyer st = new Stajyer();
+
+double toplamMaas = 0;
+toplamMaas = gm.maasinizNedir() + m.maasinizNedir() + pr.maasinizNedir() + st.maasinizNedir();
+Console.WriteLine("Toplam Maaş : " + toplamMaas);
